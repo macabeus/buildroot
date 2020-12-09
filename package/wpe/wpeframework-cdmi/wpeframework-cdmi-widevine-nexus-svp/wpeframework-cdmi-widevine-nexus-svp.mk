4fd56fcb3c67f41faaba64cbd2cd3176d8e6620c
Neeraj Deshpande <n.deshpande@metrological.com>
2020-12-09 21:25:55 +0530

    wpeframework-cdmi-widevine-nexus-svp : version bump.

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-widevine-nexus-svp/wpeframework-cdmi-widevine-nexus-svp.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-widevine-nexus-svp/wpeframework-cdmi-widevine-nexus-svp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_VERSION = 0601c7fc2b5ac5ae901688b0771f1c2342c40243
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_VERSION = bd7cb7b2e4bddd18f978ccab67342dfecf28755a
 WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_SITE_METHOD = git
-WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_SITE = git@github.com:rdkcentral/OCDM-Widevine-Nexus-SVP.git
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_SITE = https://code.rdkcentral.com/r/soc/broadcom/components/rdkcentral/OCDM-Widevine-Nexus-SVP
 WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_INSTALL_STAGING = NO
 WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_DEPENDENCIES = wpeframework-clientlibraries
 
